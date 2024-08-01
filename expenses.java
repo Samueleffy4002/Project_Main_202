d2d70c999796d0b3eb90ce5e369428aa512ab181
Samueleffy4002 <129853091+Samueleffy4002@users.noreply.github.com>
2024-08-01 19:21:27 +0100

    Delete expenses.java

--- expenses.java
+++
@@ -1,7 +0,0 @@
-Description of the Expense Class
-Our expense tracker class is a tool designed to help users manage and monitor their personal finances effectively. It enables users to keep track of their expenses, categorize spending, set budgets, and generate reports to understand their financial habits.
-The Expense class models an expense with the following attributes:
-Date: The date on which the expense was incurred.
-Amount: The amount of money spent.
-Category: The category under which the expense falls (e.g., Food, Transportation and so on).
-Description: A textual description or note about the expense.
